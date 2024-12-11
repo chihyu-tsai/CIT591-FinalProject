c33213a3b5fa3c3d99ed7541a40e6e882c20fea3
tiff <hytsai@seas.upenn.edu>
2024-12-11 14:00:17 -0500

    add JavaDoc headers for Battleship

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,10 +1,16 @@
 public class Battleship extends Ship {
 
+    /**
+     * Sets the inherited length variable and initializes the hit array, based on the size of this ship (4 tiles).
+     */
     public Battleship() {
         this.length = 4;
         this.hit = new boolean[4];
     }
 
+    /**
+     * @return "Battleship", indicating that this is a Battleship.
+     */
     @Override
     public String getShipType() {
         return "Battleship";
