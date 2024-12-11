8e25ebdf87d4c5b8afe21ae4d5d53bd0f3f6a0ef
tiff <hytsai@seas.upenn.edu>
2024-12-11 14:00:48 -0500

    add JavaDoc headers for Destroyer

--- src/Destroyer.java
+++ src/Destroyer.java
@@ -1,10 +1,16 @@
 public class Destroyer extends Ship {
 
+    /**
+     * Sets the inherited length variable and initializes the hit array, based on the size of this ship (2 tiles).
+     */
     public Destroyer() {
         this.length = 2;
         this.hit = new boolean[2];;
     }
 
+    /**
+     * @return "Destroyer", indicating that this is a Destroyer.
+     */
     @Override
     public String getShipType() {
         return "Destroyer";
