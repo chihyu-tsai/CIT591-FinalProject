899de90415f83869fa261c6f0ad69581c03b6326
tiff <hytsai@seas.upenn.edu>
2024-12-11 14:01:19 -0500

    add JavaDoc headers for Submarine

--- src/Submarine.java
+++ src/Submarine.java
@@ -1,10 +1,16 @@
 public class Submarine extends Ship{
 
+    /**
+     * Sets the inherited length variable and initializes the hit array, based on the size of this ship (1 tiles).
+     */
     public Submarine() {
         this.length = 1;
         this.hit = new boolean[1];;
     }
 
+    /**
+     * @return "Submarine", indicating that this is a Submarine.
+     */
     @Override
     public String getShipType() {
         return "Submarine";
