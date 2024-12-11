5c3f44efb3180d34fea8223d1876482cf6baf572
tiff <hytsai@seas.upenn.edu>
2024-12-11 14:00:34 -0500

    add JavaDoc headers for Cruiser

--- src/Cruiser.java
+++ src/Cruiser.java
@@ -1,10 +1,16 @@
 public class Cruiser extends Ship{
 
+    /**
+     * Sets the inherited length variable and initializes the hit array, based on the size of this ship (3 tiles).
+     */
     public Cruiser() {
         this.length = 3;
         this.hit = new boolean[3];;
     }
 
+    /**
+     * @return "Cruiser", indicating that this is a Cruiser.
+     */
     @Override
     public String getShipType() {
         return "Cruiser";
