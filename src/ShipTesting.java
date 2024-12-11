b74c43565dbf7f67ac911064aa3b290bde5c097b
tiff <hytsai@seas.upenn.edu>
2024-12-11 13:03:43 -0500

    update testGetBowRow(), testGetBowColumn(), testSetBowColumn()

--- src/ShipTesting.java
+++ src/ShipTesting.java
@@ -39,37 +39,58 @@ public class ShipTesting {
         Ocean ocean = new Ocean();
         ship1.placeShipAt(1, 2, true, ocean);
         assertEquals(1, ship1.getBowRow());
-        ship1.placeShipAt(2, 5, true, ocean);
-        assertEquals(2, ship1.getBowRow());
-        ship1.placeShipAt(4, 3, true, ocean);
-        assertEquals(4, ship1.getBowRow());
-        ship1.placeShipAt(7, 2, true, ocean);
-        assertEquals(7, ship1.getBowRow());
+        ship2.placeShipAt(2, 5, true, ocean);
+        assertEquals(2, ship2.getBowRow());
+        ship3.placeShipAt(4, 3, true, ocean);
+        assertEquals(4, ship3.getBowRow());
+        ship4.placeShipAt(7, 2, true, ocean);
+        assertEquals(7, ship4.getBowRow());
     }
 
 
     @Test
     void testGetBowColumn() {
-        Ship ship = new Battleship();
+        Ship ship1 = new Battleship();
+        Ship ship2 = new Cruiser();
+        Ship ship3 = new Destroyer();
+        Ship ship4 = new Submarine();
 
         // Test initial state
-        assertEquals(0, ship.getBowColumn());
+        assertEquals(0, ship1.getBowRow());
+        assertEquals(0, ship2.getBowRow());
+        assertEquals(0, ship3.getBowRow());
+        assertEquals(0, ship4.getBowRow());
 
         // Test after placing ship
         Ocean ocean = new Ocean();
-        ship.placeShipAt(2, 4, true, ocean);
-        assertEquals(4, ship.getBowColumn());
+        ship1.placeShipAt(1, 2, true, ocean);
+        assertEquals(2, ship1.getBowColumn());
+        ship2.placeShipAt(9, 1, true, ocean);
+        assertEquals(1, ship2.getBowColumn());
+        ship3.placeShipAt(4, 3, true, ocean);
+        assertEquals(3, ship3.getBowColumn());
+        ship4.placeShipAt(7, 2, true, ocean);
+        assertEquals(2, ship4.getBowColumn());
     }
 
     @Test
     void testSetBowColumn() {
-        Ship ship = new Battleship();
+        Ship ship1 = new Battleship();
+        Ship ship2 = new Cruiser();
+        Ship ship3 = new Destroyer();
+        Ship ship4 = new Submarine();
 
-        ship.setBowColumn(1);
-        assertEquals(1, ship.getBowColumn());
+        ship1.setBowColumn(1);
+        assertEquals(1, ship1.getBowColumn());
 
-        ship.setBowColumn(3);
-        assertEquals(3, ship.getBowColumn());
+        ship2.setBowColumn(3);
+        assertEquals(3, ship2.getBowColumn());
+
+        ship3.setBowColumn(5);
+        assertEquals(5, ship3.getBowColumn());
+
+        ship4.setBowColumn(7);
+        assertEquals(7, ship4.getBowColumn());
 
     }
 
